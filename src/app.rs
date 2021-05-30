e94ecc2e7bdc5087ddfc2e380eff5387679f0e0f
Yap Jia Hong <yapjiahong2003@gmail.com>
2021-05-30 13:14:15 +0800

    'initial' commit, I kept forgetting to create a repository.

---
+++ src/app.rs
@@ -0,0 +1,137 @@
+use eframe::{egui, epi};
+use egui::widgets;
+use egui::TextStyle;
+
+pub struct App {
+    display: String,
+    answer: usize
+}
+
+impl Default for App {
+    fn default() -> Self {
+        App {
+            display: "".to_string(),
+            answer: 0
+        }
+    }
+}
+
+// TODO: Reading list
+// https://createlang.rs/
+// https://fenga.medium.com/how-to-build-a-calculator-bf558e6bd8eb
+// https://www.twilio.com/blog/abstract-syntax-trees
+fn parse_calculation(s: &str) -> String {
+    let operators = "+-÷×";
+    for op in operators.chars() {
+       if s.contains(op) {
+           s.split(op);
+       }
+    }
+    String::from("Hello")
+}
+
+fn button(ui: &mut egui::Ui, text: &str) -> egui::Response {
+    let button = egui::Button::new(text);
+    let button = ui.add_sized(egui::Vec2 { x: 40.0, y: 40.0 }, button);
+    button
+}
+
+impl epi::App for App {
+    fn name(&self) -> &str {
+        "Kal"
+    }
+
+    // TODO: Add a vertical spacer to spread the buttons out.
+    // Also look at button_padding
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        let Self { display, answer } = self;
+
+        egui::TopPanel::top("Calculations").show(ctx, |ui| {
+            ui.add_sized(
+                egui::Vec2 { x: 240.0, y: 80.0 },
+                widgets::Label::new(&display),
+            )
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if button(ui, "1").clicked() {
+                    *display = format!("{}{}", display, "1".to_string());
+                }
+                if button(ui, "2").clicked() {
+                    *display = format!("{}{}", display, "2".to_string());
+                }
+                if button(ui, "3").clicked() {
+                    *display = format!("{}{}", display, "3".to_string());
+                }
+                if button(ui, "+").clicked() {
+                    *display = format!("{}{}", display, "+".to_string());
+                }
+                if button(ui, "-").clicked() {
+                    *display = format!("{}{}", display, "-".to_string());
+                }
+            });
+
+            ui.horizontal(|ui| {
+                if button(ui, "4").clicked() {
+                    *display = format!("{}{}", display, "4".to_string());
+                }
+
+                if button(ui, "5").clicked() {
+                    *display = format!("{}{}", display, "5".to_string());
+                }
+
+                if button(ui, "6").clicked() {
+                    *display = format!("{}{}", display, "6".to_string());
+                }
+
+                if button(ui, "÷").clicked() {
+                    *display = format!("{}{}", display, "÷".to_string());
+                }
+
+                if button(ui, "×").clicked() {
+                    *display = format!("{}{}", display, "×".to_string());
+                }
+            });
+
+            ui.horizontal(|ui| {
+                if button(ui, "7").clicked() {
+                    *display = format!("{}{}", display, "7".to_string());
+                }
+
+                if button(ui, "8").clicked() {
+                    *display = format!("{}{}", display, "8".to_string());
+                }
+
+                if button(ui, "9").clicked() {
+                    *display = format!("{}{}", display, "9".to_string());
+                };
+
+                if button(ui, "%").clicked() {
+                    *display = format!("{}{}", display, "%".to_string());
+                }
+
+                if button(ui, "AC").clicked() {
+                    display.clear();
+                }
+            });
+
+            ui.horizontal(|ui| {
+                button(ui, "");
+                if button(ui, "0").clicked() {
+                    *display = format!("{}{}", display, "0".to_string());
+                }
+                if button(ui, ".").clicked() {
+                    *display = format!("{}{}", display, ".".to_string());
+                }
+                let button = egui::Button::new("=");
+                let calculate = ui.add_sized(egui::Vec2 { x: 89.0, y: 40.0 }, button);
+                if calculate.clicked() {
+                    // TODO: Parse display string.
+                    parse_calculation(display);
+                    display.clear();
+                }
+            });
+        });
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod app;
+use app::App;
+
+use egui;
+use std::collections::HashMap;
+
+fn spawn() {
+    let app = App::default();
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::Vec2 { x: 300.0, y: 300.0 }),
+        resizable: false,
+        ..eframe::NativeOptions::default()
+    };
+    eframe::run_native(Box::new(app), options);
+}
+
+fn main() {
+    spawn();
+}
