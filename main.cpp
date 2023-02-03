10934579002c037d41179f37fe8b130fe88606bb
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-02-03 20:01:15 +0800

    Started working on a tokenizer

--- main.cpp
+++ main.cpp
@@ -7,6 +7,53 @@ const char ADD = '+';
 const char SUB = '-';
 const char MULTI = '*';
 
+class Lexer {
+    private:
+        std::vector<char> m_chars;
+        std::vector<char> m_tokens;
+        std::string m_contents;
+
+        void split_by_char() {
+            int length = m_contents.length();
+            const char* chars = m_contents.c_str();
+
+            for (int i = 0; i < length; i++) {
+                char c = chars[i];
+                if (c == ' ') {
+                    continue;
+                }
+
+                m_chars.push_back(chars[i]);
+            }
+        }
+
+        void tokenize() {
+            split_by_char();
+            int size = std::size(m_chars);
+            for (int i = 0; i < size; i++) {
+                auto c = m_chars[i];
+                char token;
+                if (c == DIVIDE) {
+                    token = DIVIDE;
+                } else if (c == ADD) {
+                    token = ADD;
+                } else if (c == MULTI) {
+                    token = MULTI;
+                } else {
+                    token = SUB;
+                }
+
+                m_tokens.push_back(token);
+            }
+        }
+
+    public:
+        Lexer(std::string content) {
+            m_contents = content;
+            tokenize();
+        }
+};
+
 void match(const char& c) {
     if (c == DIVIDE) {
     }
@@ -14,16 +61,6 @@ void match(const char& c) {
 }
 
 int main() {
-    std::string input = "";
-    getline(std::cin, input);
-
-    // Number of chars without the termination character
-    int size = input.size();
-    // Characters with the termination character
-    const char* characters = input.c_str();
-
-    for (int i = 0; i < size; i++) {
-        char character = characters[i];
-        std::cout << character << std::endl;
-    }
+    std::string line = "1 + 2 / 5 * 3";
+    auto lexer = Lexer(line);
 }
