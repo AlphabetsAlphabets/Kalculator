d92e0190937baa010512478090fce261e101bec5
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-02-13 21:33:52 +0800

    Untracked files in .gitignore

--- build/CMakeFiles/calc.dir/src/token.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/calc.dir/src/token.cpp.o: \
- /home/yjh/Documents/Kalculator/src/token.cpp /usr/include/stdc-predef.h \
- /home/yjh/Documents/Kalculator/src/token.h
--- build/CMakeFiles/calc.dir/token.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/calc.dir/token.cpp.o: /home/yjh/Documents/Kalculator/token.cpp \
- /usr/include/stdc-predef.h /home/yjh/Documents/Kalculator/token.h
